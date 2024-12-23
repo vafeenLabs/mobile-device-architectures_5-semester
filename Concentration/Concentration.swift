967f97e6c0a578271717fdfd2ba49b39aeb8a76b
Vafeen <666av6@gmail.com>
2024-12-23 21:25:16 +0300

    23.12.2024 21:25:16

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -1,88 +1,114 @@
-//
-//  Concentration.swift
-//  Concentration
-//
-//  Created by nadya on 14.10.2023.
-//
-
 import Foundation
 
+// Структура для игры
 struct Concentration {
     
+    // Массив карточек, доступных в игре
     private (set) var cards = [Card]()
     
+    // Счет игры
     var score = 0
     
+    // Флаг, который указывает, была ли использована подсказка
     private (set) var isHintUsed = false
     
+    // Индекс одной перевернутой карточки
     private var indexOfOneAndOnlyFaceUpCard: Int? {
         
+        // Геттер: Получаем индекс карточки, которая перевернута лицом вверх
         get {
-            return cards.indices.filter { cards[$0].isFaceUp }.oneAndOnly
+            return cards.indices.filter { cards[$0].isFaceUp }.oneAndOnly 
         }
         
+        // Сеттер: Устанавливаем одну перевернутую карточку
         set {
+            // Переворачиваем только ту карточку, которая соответствует новому значению индекса
             for index in cards.indices {
-                cards[index].isFaceUp = (index == newValue)
+                cards[index].isFaceUp = (index == newValue) 
             }
         }
     }
     
+    // Метод для выбора карточки
     mutating func chooseCard(at index: Int) {
-        assert(cards.indices.contains(index), "Concentration.chooseCard(at: \(index)): choosen index not in the cards")
+        // Проверка, что индекс карточки валиден (находится в пределах массива)
+        assert(cards.indices.contains(index), "Concentration.chooseCard(at: \(index)): chosen index not in the cards")
+        
+        // Проверяем, что карточка не совпала (не имеет метки isMatched)
         if !cards[index].isMatched {
+            
+            // Если есть одна перевернутая карточка, то пытаемся найти пару
             if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                
+                // Если карточки совпали, помечаем их как совпавшие
                 if cards[matchIndex] == cards[index] {
-                    cards[matchIndex].isMatched = true
-                    cards[index].isMatched = true
-                    score += 1
-                    cards[index].isGuessed = true
-                    cards[matchIndex].isGuessed = true
+                    cards[matchIndex].isMatched = true  // Помечаем первую карточку как совпавшую
+                    cards[index].isMatched = true      // Помечаем вторую карточку как совпавшую
+                    score += 1  // Увеличиваем счет на 1
+                    cards[matchIndex].isGuessed = true // Помечаем первую карточку как угаданную
+                    cards[index].isGuessed = true      // Помечаем вторую карточку как угаданную
+                    
                 } else {
-                    score -= 2
+                    score -= 2  // Если карточки не совпали, уменьшаем счет на 2
                 }
+                
+                // Переворачиваем выбранную карточку лицом вверх
                 cards[index].isFaceUp = true
-                indexOfOneAndOnlyFaceUpCard = nil
+                
+                // Сбрасываем индекс перевернутой карточки
+                indexOfOneAndOnlyFaceUpCard = nil 
             } else {
-                indexOfOneAndOnlyFaceUpCard = index
+                // Если нет перевернутой карточки, переворачиваем текущую
+                indexOfOneAndOnlyFaceUpCard = index 
             }
         }
     }
     
+    // Метод для переворачивания всех карточек лицом вверх
     mutating func flipAllCardsFaceUp() {
+        // Проходим по всем карточкам и переворачиваем их лицом вверх, если они не совпали
         for index in cards.indices {
             if !cards[index].isMatched {
-                cards[index].isFaceUp = true
+                cards[index].isFaceUp = true 
             }
         }
     }
     
+    // Метод для переворачивания всех карточек лицом вниз
     mutating func flipAllCardsFaceDown() {
+        // Проходим по всем карточкам и переворачиваем их лицом вниз
         for index in cards.indices {
-            cards[index].isFaceUp = false
+            cards[index].isFaceUp = false 
         }
     }
     
+    // Метод для перемешивания карточек
     mutating func shuffleCards() {
-        cards.shuffle()
+        // Перемешиваем массив карточек случайным образом
+        cards.shuffle() 
     }
     
+    // Метод для использования подсказки
     mutating func useHint() {
-        isHintUsed = true
+        isHintUsed = true  // Помечаем, что подсказка была использована 
     }
     
+    // Инициализатор для создания игры с заданным количеством пар карточек
     init(numberOfPairsOfCards: Int) {
-        assert(numberOfPairsOfCards > 0, "Concentration.init(\(numberOfPairsOfCards)): tou must at least one pair of cards")
-        for _ in 1...numberOfPairsOfCards {
-            let card = Card()
-            cards += [card, card]
+        assert(numberOfPairsOfCards > 0, "Concentration.init(\(numberOfPairsOfCards)): must have at least one pair of cards") 
+       
+        for _ in 1...numberOfPairsOfCards { 
+            let card = Card()  // Создаем новую карточку 
+            cards += [card, card]  // Добавляем пару одинаковых карточек в массив 
         }
-        cards.shuffle()
+        
+        cards.shuffle()  // Перемешиваем карточки, чтобы они не шли подряд 
     }
 }
 
-extension Collection {
-    var oneAndOnly: Element? {
-        return count == 1 ? first : nil
-    }
+// Расширение для коллекций, чтобы получить единственный элемент 
+extension Collection { 
+    var oneAndOnly: Element? { 
+        return count == 1 ? first : nil  // Если коллекция состоит из одного элемента, возвращаем его 
+    } 
 }
